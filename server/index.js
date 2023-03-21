309112676941fcfeb79f04a2cd4bfdba1a7fc963
IsaacNuttall <isaacnuttall@gmail.com>
2023-03-21 17:24:09 -0600

    nodemon

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.use(express.json())
+
+const {getTasks} = require('./controller.js')
+
+app.get('/api/tasks', getTasks)
+
+app.listen(6789, () => console.log('Docked at port 6789.'))
